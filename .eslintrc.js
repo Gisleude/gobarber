d7a4b8097329ce0c0a125ccdea06ceda372f3fd2
Gisleude Linhares <gisleudenem@live.com>
2019-11-03 14:48:25 -0300

    configurações redis + horários disponíveis

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+    "no-console": "off",
   },
 };
